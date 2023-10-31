b8722a4d94713180debefa5fdb95282382c03e53
juan-20 <juanalvesandrade@gmail.com>
2023-10-31 19:14:14 -0300

    adding i18n to the website

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: "English",
+  br: "Português",
+};
+
+export const deafultLanguage = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+  },
+} as const;
