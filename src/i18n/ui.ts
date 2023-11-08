6f7593497cc47002a6a70b0cc89f535b92405f4f
juan-20 <juanalvesandrade@gmail.com>
2023-11-08 10:07:49 -0300

    initial page done :tada:

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,8 +14,21 @@ export const ui = {
     "nav.text.work": "Work",
     "nav.text.about": "About",
 
+    "skills.first": "Full Stack",
+    "skills.first.about":
+      "Knowledge in web application development, from front-end to back-end.",
+    "skills.second": "Cooperative",
+    "skills.second.about":
+      "Communication in a clear and objective way forming a collective sense",
+    "skills.third": "Problem-solver",
+    "skills.third.about":
+      "Ability to solve problems and find solutions in a creative way.",
+
     "about.title": "About",
     "about.text": "This is a simple example of react-i18next.",
+
+    "contact.title": "Interested in working together?",
+    "contact.button": "Send me a message",
   },
   pt: {
     "nav.link.home": "/pt",
@@ -25,8 +38,21 @@ export const ui = {
     "nav.text.work": "Trabalhos",
     "nav.text.about": "Sobre",
 
+    "skills.first": "Full Stack",
+    "skills.first.about":
+      "Conhecimento em desenvolvimento de aplicações web, desde o front-end até o back-end.",
+    "skills.second": "Cooperativo",
+    "skills.second.about":
+      "Comunicação de forma clara e objetiva formando um senso coletivo",
+    "skills.third": "Resolver problemas",
+    "skills.third.about":
+      "Capacidade de resolver problemas e encontrar soluções de forma criativa.",
+
     "about.title": "Sobre",
     "about.text":
       "Leia abaixo para saber mais sobre meu background e experiência profissional.",
+
+    "contact.title": "Interessado em trabalhar juntos?",
+    "contact.button": "Mande uma mensagem",
   },
 } as const;
